75d36422af48e3f3a2ea19922fb57e70f92e3050
DEV_Hao <102379977+Phihao2k3@users.noreply.github.com>
2024-05-17 14:34:03 +0700

    1.7.1 update icon

--- src/app/pages/product/List-product/product-list.compoment.ts
+++ src/app/pages/product/List-product/product-list.compoment.ts
@@ -8,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class productlistComponent {
   settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
     columns: {
       ID: {
         title: 'ID',
