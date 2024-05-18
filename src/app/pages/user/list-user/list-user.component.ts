e362ff07a0cb2b5c6934215125e2d7d2c0e4ec46
Duongbro <nguyenduongthai000@gmail.com>
2024-05-18 22:01:11 +0700

    1.7 Giao diện bảng User [Update]

--- src/app/pages/user/list-user/list-user.component.ts
+++ src/app/pages/user/list-user/list-user.component.ts
@@ -8,24 +8,91 @@ import { NbSortDirection, NbSortRequest, NbTreeGridDataSource, NbTreeGridDataSou
   styleUrls: ['./list-user.component.scss'],
 })
 export class ListUserComponent {
- 
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      ID: {
+        title: 'ID',
+      },
+      username: {
+        title: 'Tên đăng nhập',
+      },
+      password: {
+        title: 'Mật khẩu',
+      },
+      first: {
+        title: 'Họ',
+      },
+      last: {
+        title: 'Tên',
+      },
+      email: {
+        title: 'Email',
+      },
+      role: {
+        title: 'Vai trò', 
+      },
+      
+    },
+    actions: {
+      // Define actions column
+      title: 'Actions',
+      type: 'html',
+      filter: false,
+      sort: false,
+    },
+  };
+
+  data = [
+    {
+      ID: 1,
+      username: 'nthaiduong00000',
+      password: 'teketeke123',
+      first: 'Nguyễn',
+      last: 'Thái Dương',
+      email: 'duonghehe@gmail.com',
+      role: 'Nhân viên',
+    },
+    {
+      ID: 2,
+      username: 'haodomdom',
+      password: 'jack97',
+      first: 'Trần',
+      last: 'Phi Hào',
+      email: 'haodomdom97@gmail.com',
+      role: 'Khách hàng',
+    },
+  ];
 }
 
-@Component({
-  selector: 'ngx-fs-icon',
-  template: `
-    <nb-tree-grid-row-toggle [expanded]="expanded" *ngIf="isDir(); else fileIcon">
-    </nb-tree-grid-row-toggle>
-    <ng-template #fileIcon>
-      <nb-icon icon="file-text-outline"></nb-icon>
-    </ng-template>
-  `,
-})
-export class FsIconComponent {
-  @Input() kind: string;
-  @Input() expanded: boolean;
+// @Component({
+//   selector: 'ngx-fs-icon',
+//   template: `
+//     <nb-tree-grid-row-toggle [expanded]="expanded" *ngIf="isDir(); else fileIcon">
+//     </nb-tree-grid-row-toggle>
+//     <ng-template #fileIcon>
+//       <nb-icon icon="file-text-outline"></nb-icon>
+//     </ng-template>
+//   `,
+// })
+// export class FsIconComponent {
+//   @Input() kind: string;
+//   @Input() expanded: boolean;
 
-  isDir(): boolean {
-    return this.kind === 'dir';
-  }
-}
+//   isDir(): boolean {
+//     return this.kind === 'dir';
+//   }
+// }
